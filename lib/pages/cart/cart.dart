abd0fc8c04b0e8705ee6dcb37d9ec5e32b2dbca3
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-12 09:05:05 +0800

    Basic Cart

--- lib/pages/cart/cart.dart
+++ lib/pages/cart/cart.dart
@@ -1,14 +1,134 @@
 import 'package:flutter/material.dart';
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+class CartPage extends StatefulWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  _MyCartState createState() => _MyCartState();
+}
+
+class _MyCartState extends State<CartPage> {
+  List<Map<String, dynamic>> cartItems = [
+    {'name': 'Item 1', 'quantity': 1},
+    {'name': 'Item 2', 'quantity': 2},
+    {'name': 'Item 3', 'quantity': 3},
+  ];
+
+  void _removeItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Delete'),
+          content: const Text('Are you sure you want to delete this item from your cart?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  cartItems.removeAt(index);
+                });
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _increaseQuantity(int index) {
+    setState(() {
+      cartItems[index]['quantity']++;
+    });
+  }
+
+  void _decreaseQuantity(int index) {
+    setState(() {
+      if (cartItems[index]['quantity'] > 1) {
+        cartItems[index]['quantity']--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Cart"),
+        title: const Text('Cart'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: cartItems.isEmpty
+                ? const Center(
+              child: Text('Your cart is empty.'),
+            )
+                : ListView.builder(
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                final item = cartItems[index];
+                return ListTile(
+                  title: Row(
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(item['name']),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: () => _decreaseQuantity(index),
+                      ),
+                      Text('${item['quantity']}'),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () => _increaseQuantity(index),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _removeItem(index),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                print('Checkout button pressed');
+              },
+              child: Text('Checkout'),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: CartPage(),
+  ));
 }
