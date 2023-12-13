fa933701f196ffe7659bd24c3ff2c93b7eeaf816
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-13 08:54:34 +0800

    Basic Profile

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,14 +1,115 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile"),
+        title: const Text('Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/133362311?v=4',
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'edions',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              SizedBox(height: 20),
+              // Add an icon before the text
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.location_on),
+                  SizedBox(width: 8), // Adjust the spacing as needed
+                  Text('Anywhere at the same time'),
+                ],
+              ),
+              SizedBox(height: 8), // Adjust the spacing as needed
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone),
+                  SizedBox(width: 8), // Adjust the spacing as needed
+                  Text('+9000'),
+                ],
+              ),
+              SizedBox(height: 20),
+              // Use a Container instead of ElevatedButton
+              Container(
+                width: double.infinity, // Take up the full width
+                padding: EdgeInsets.symmetric(vertical: 16),
+                decoration: BoxDecoration(
+                  color: Colors.grey, // Customize the button color
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    // Add logic for editing profile
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.edit, color: Colors.white),
+                      SizedBox(width: 8),
+                      Text(
+                        'Edit Profile',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // Use a Container instead of ElevatedButton
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                decoration: BoxDecoration(
+                  color: Colors.grey, // Customize the button color
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    // Add logic for logging out
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.logout, color: Colors.white),
+                      SizedBox(width: 8),
+                      Text(
+                        'Logout',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ProfilePage(),
+    ),
+  );
 }
