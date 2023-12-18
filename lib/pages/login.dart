a959fdc4242013c640e7a09bdd9407912d43fb6d
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-18 09:34:18 +0800

    Adjustment on login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_ordering/widgets/navigation.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _RectanglePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _RectanglePageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> {
   TextEditingController controller1 = TextEditingController();
   TextEditingController controller2 = TextEditingController();
-  double? result;
 
-  void calculateArea() {
-    // double length = double.tryParse(controller1.text) ?? 0;
-    // double width = double.tryParse(controller2.text) ?? 0;
-    //
-    // double area = length * width;
-    //
-    // setState(() {
-    //   result = area;
-    // });
+  // Hardcoded credentials for simplicity (replace with actual authentication logic)
+  final String validEmail = "user";
+  final String validPassword = "123";
+
+  void processLogin() {
+    String enteredEmail = controller1.text;
+    String enteredPassword = controller2.text;
+
+    if (enteredEmail == validEmail && enteredPassword == validPassword) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const NavigationWidget()),
+      );
+    } else {
+      // Unsuccessful login
+      print("Login failed. Invalid credentials.");
+      // showToast("Invalid credentials");
+    }
   }
 
+  // void showToast(String message) {
+  //   Fluttertoast.showToast(
+  //     msg: message,
+  //     toastLength: Toast.LENGTH_SHORT,
+  //     gravity: ToastGravity.BOTTOM,
+  //     timeInSecForIosWeb: 1,
+  //     backgroundColor: Colors.red,
+  //     textColor: Colors.white,
+  //     fontSize: 16.0,
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +55,14 @@ class _RectanglePageState extends State<LoginPage> {
             const Text(
               "Login",
               style: TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.bold),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            SizedBox(
-              height: 50,
-            ),
+            SizedBox(height: 50),
             TextField(
               controller: controller1,
-              keyboardType: TextInputType.number,
+              keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
                 labelText: "Email",
                 border: OutlineInputBorder(
@@ -49,12 +70,10 @@ class _RectanglePageState extends State<LoginPage> {
                 ),
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 20),
             TextField(
               controller: controller2,
-              keyboardType: TextInputType.number,
+              obscureText: true, // Hide password
               decoration: InputDecoration(
                 labelText: "Password",
                 border: OutlineInputBorder(
@@ -62,12 +81,10 @@ class _RectanglePageState extends State<LoginPage> {
                 ),
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                calculateArea();
+                processLogin();
                 controller1.clear();
                 controller2.clear();
               },
