26edb372be1738edf34ff067524de5a68210079f
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-19 11:45:07 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_food_ordering/pages/login.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: const LoginPage(),
-    title: "Food Ordering",
-    theme: ThemeData(
-      primarySwatch: Colors.lightGreen,
-    ),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Ordering',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.lightGreen,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
